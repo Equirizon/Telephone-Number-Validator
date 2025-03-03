4d7328abd7b204f31a032e658e5590d26972955f
Equirizon <equirizon@gmail.com>
2025-03-03 21:45:04 +0800

    added styling

--- script.js
+++ script.js
@@ -39,15 +39,48 @@ const invalidUSNumbers = [
     '11 555-555-5555'
 ];
 
+
+const logoAnimation = () => {
+    const logo = document.querySelector('.logo');
+    console.log(logo);
+    logo.addEventListener('mouseenter', () => {
+        logo.classList.add('hover');
+    })
+    logo.addEventListener('mouseleave', () => {
+        const hover = document.querySelector('.hover');
+        const computedStyle = window.getComputedStyle(hover);
+        const hue = computedStyle.filter;
+        logo.style.setProperty('filter', hue)
+        setTimeout(() => {
+            logo.style.setProperty('filter',`hue-rotate(0deg)`)
+        }, 1);
+        logo.classList.remove('hover');
+    });
+}
+
+const buttonAnimation = () => {
+    const button = document.querySelectorAll('button');
+    button.forEach((button) => {
+        button.addEventListener('mousedown', () => {
+            button.classList.add("active");
+            setTimeout(() => {
+                button.classList.remove("active");
+            }, 400);
+        });
+    });
+}
+
+buttonAnimation();
+logoAnimation();
+
 const clearInput = () => {
-if (input.value) {
     input.value = '';
-    result.removeAttribute('class');
+    result.classList.remove('valid');
+    result.classList.remove('invalid');
     result.textContent = 'Output was cleared';
     setTimeout(() => {
-        result.textContent = '';
+        result.classList.add('collapse');
     }, 1000);
-}
 };
 
 const telephoneCheckRegexUS = (str) => {
@@ -55,33 +88,34 @@ const telephoneCheckRegexUS = (str) => {
     return regex.test(str);
 };
 
-console.log('--- Valid Numbers (true) ---');
-validUSNumbers.forEach(number => {
-    console.log(number, telephoneCheckRegexUS(number));
-    isValidCount += 1;
-});
+// console.log('--- Valid Numbers (true) ---');
+// validUSNumbers.forEach(number => {
+//     console.log(telephoneCheckRegexUS(number),number);
+//     isValidCount += 1;
+// });
 
-console.log('--- Invalid Numbers (false) ---');
-invalidUSNumbers.forEach(number => {
-    console.log(number, telephoneCheckRegexUS(number));
-    isInvalidCount += 1;
-});
+// console.log('--- Invalid Numbers (false) ---');
+// invalidUSNumbers.forEach(number => {
+//     console.log(telephoneCheckRegexUS(number), number);
+//     isInvalidCount += 1;
+// });
 
-isValid = validUSNumbers.every(number => telephoneCheckRegexUS(number))
-isInvalid = invalidUSNumbers.every(number => !telephoneCheckRegexUS(number))
+// isValid = validUSNumbers.every(number => telephoneCheckRegexUS(number))
+// isInvalid = invalidUSNumbers.every(number => !telephoneCheckRegexUS(number))
 
-console.log(`Valid Test(${isValidCount}): ${isValid ? 'PASS' : 'FAIL'}, Invalid Test(${isInvalidCount}): ${isInvalid ? 'PASS' : 'FAIL'}`)
+// console.log(`Valid Test(${isValidCount}): ${isValid ? 'PASS' : 'FAIL'}, Invalid Test(${isInvalidCount}): ${isInvalid ? 'PASS' : 'FAIL'}`)
 
 const checkNumber = () => {
     if (input.value === '') {
         alert('Please provide a phone number');
         } else {
-            if (telephoneCheckRegexUS(input.value)) {
+            result.classList.remove('collapse','hidden');
+            if (telephoneCheckRegexUS(input.value.trim())) {
                 result.textContent = `Valid US number: ${input.value}`
-                result.matches('.invalid') ? result.classList.replace('invalid','valid') : result.classList.add('valid');
+                result.classList.replace('invalid','valid') || result.classList.add('valid');
             } else {
                 result.textContent = `Invalid US number: ${input.value}`;
-                result.matches('.valid') ? result.classList.replace('valid','invalid') : result.classList.add('invalid');
+                result.classList.replace('valid','invalid') || result.classList.add('invalid');
             }
         }
 }
